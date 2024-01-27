ee1b1f791792bfc2a1598867a076bc516c180524
Michael Cousins <michael.cousins@viam.com>
2024-01-28 07:11:41 +1100

    feat(vitest): add vitest cleanup export (#294)
    feat(vitest): add vitest cleanup import

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
-    test: {
-        environment: 'jsdom',
-        setupFiles: ['./src/test-setup.js'],
+  plugins: [svelte()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/_vitest-setup.js'],
+    coverage: {
+      provider: 'v8',
+      include: ['src'],
     },
+  },
 })
