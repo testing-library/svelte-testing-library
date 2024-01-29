3a6cd67b3b68b7fdfde5a199f6a15a96255f96cc
Michael Cousins <michael.cousins@viam.com>
2024-01-30 10:39:10 +1100

    test: add onMount test and replace onDestory test

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [svelte()],
+  resolve: {
+    conditions: mode === 'test' ? ['browser'] : [],
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/__tests__/_vitest-setup.js'],
+    mockReset: true,
+    unstubGlobals: true,
     coverage: {
       provider: 'v8',
       include: ['src'],
     },
   },
-})
+}))
