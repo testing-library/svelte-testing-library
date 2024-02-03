cc68c261db98de1dfd263d436378f42778fce4db
Michael Cousins <michael.cousins@viam.com>
2024-02-03 11:35:52 +1000

    test(transitions): add on:introend test

---
+++ src/__tests__/fixtures/Transitioner.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { blur } from 'svelte/transition'
+
+  let show = false
+  let introDone = false
+</script>
+
+<button on:click={() => (show = true)}>Show</button>
+
+{#if show}
+  <div in:blur={{ duration: 64 }} on:introend={() => (introDone = true)}>
+    {#if introDone}
+      <p data-testid="intro-done">Done</p>
+    {:else}
+      <p data-testid="intro-pending">Pending</p>
+    {/if}
+  </div>
+{/if}
---
+++ src/__tests__/transition.test.js
@@ -0,0 +1,30 @@
+import { userEvent } from '@testing-library/user-event'
+import { beforeEach, describe, expect, test, vi } from 'vitest'
+
+import { render, screen, waitFor } from '..'
+import Transitioner from './fixtures/Transitioner.svelte'
+
+describe('transitions', () => {
+  beforeEach(() => {
+    if (window.navigator.userAgent.includes('jsdom')) {
+      const raf = (fn) => setTimeout(() => fn(new Date()), 16)
+      vi.stubGlobal('requestAnimationFrame', raf)
+    }
+  })
+
+  test('on:introend', async () => {
+    const user = userEvent.setup()
+
+    render(Transitioner)
+    const start = screen.getByRole('button')
+    await user.click(start)
+
+    const pending = screen.getByTestId('intro-pending')
+    expect(pending).toBeInTheDocument()
+
+    await waitFor(() => {
+      const done = screen.queryByTestId('intro-done')
+      expect(done).toBeInTheDocument()
+    })
+  })
+})
