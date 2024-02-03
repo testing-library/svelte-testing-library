8bf0f9689fed6038feccfb4dd3fcc2060bcb029e
Yanick Champoux <yanick@babyl.ca>
2024-02-03 13:09:31 -0500

    add rerender test

--- src/__tests__/rerender.test.js
+++ src/__tests__/rerender.test.js
@@ -1,16 +1,28 @@
 /**
  * @jest-environment jsdom
  */
-import { describe, expect, test } from 'vitest'
+import { expect, test } from 'vitest'
+import { writable } from 'svelte/store'
 
-import { render } from '..'
-import Comp from './fixtures/Comp.svelte'
+import { render, waitFor } from '..'
+import Comp from './fixtures/Rerender.svelte'
 
-describe('rerender', () => {
-  test('rerenders component with newly set prop', async () => {
-    const { container, rerender } = render(Comp, { props: { name: 'World 1' } })
-    expect(container.firstChild).toHaveTextContent('Hello World 1!')
-    await rerender({ props: { name: 'World 2' } })
-    expect(container.firstChild).toHaveTextContent('Hello World 2!')
+const mountCounter = writable(0)
+
+test('mounts new component successfully', async () => {
+  const { getByTestId, rerender } = render(Comp, {
+    props: { name: 'World 1' },
+    context: new Map(Object.entries({ mountCounter })),
+  })
+
+  await waitFor(() => {
+    expect(getByTestId('test')).toHaveTextContent('Hello World 1!')
+    expect(getByTestId('mount-counter')).toHaveTextContent('1')
+  })
+
+  rerender({ props: { name: 'World 2' } })
+  await waitFor(() => {
+    expect(getByTestId('test')).toHaveTextContent('Hello World 2!')
+    expect(getByTestId('mount-counter')).toHaveTextContent('1')
   })
 })
