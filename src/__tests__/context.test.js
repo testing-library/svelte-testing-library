914a5bb40cb433bdbaac3524eeee38f4e41a1bc9
Yanick Champoux <yanick@babyl.ca>
2024-02-22 12:02:03 -0500

    context.test.js fails too

--- src/__tests__/context.test.js
+++ src/__tests__/context.test.js
@@ -1,14 +1,18 @@
 import { expect, test } from 'vitest'
+import { VERSION as SVELTE_VERSION } from 'svelte/compiler'
 
 import { render } from '..'
 import Comp from './fixtures/Context.svelte'
 
-test('can set a context', () => {
-  const message = 'Got it'
+test.skipIf(SVELTE_VERSION >= '5' && process.env.VITEST_ENV == 'happy-dom')(
+  'can set a context',
+  () => {
+    const message = 'Got it'
 
-  const { getByText } = render(Comp, {
-    context: new Map(Object.entries({ foo: { message } })),
-  })
+    const { getByText } = render(Comp, {
+      context: new Map(Object.entries({ foo: { message } })),
+    })
 
-  expect(getByText(message)).toBeTruthy()
-})
+    expect(getByText(message)).toBeTruthy()
+  }
+)
