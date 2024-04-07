8d8a8d737251013f72ffb12622f7f50ebc61a379
Michael Cousins <michael@cousins.io>
2024-04-07 11:33:04 -0400

    test: remove Svelte 5 + HappyDOM skips after bugfix

--- src/__tests__/context.test.js
+++ src/__tests__/context.test.js
@@ -2,9 +2,8 @@ import { render } from '@testing-library/svelte'
 import { expect, test } from 'vitest'
 
 import Comp from './fixtures/Context.svelte'
-import { IS_HAPPYDOM, IS_SVELTE_5 } from './utils.js'
 
-test.skipIf(IS_SVELTE_5 && IS_HAPPYDOM)('can set a context', () => {
+test('can set a context', () => {
   const message = 'Got it'
 
   const { getByText } = render(Comp, {
