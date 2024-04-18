e951b9a7f1e233f2204bf2be96924d3047ff31dd
Michael Cousins <michael@cousins.io>
2024-04-18 11:11:09 -0400

    test: remove Svelte 5 + HappyDOM skips after bugfix (#348)

--- src/__tests__/mount.test.js
+++ src/__tests__/mount.test.js
@@ -2,13 +2,12 @@ import { act, render, screen } from '@testing-library/svelte'
 import { describe, expect, test, vi } from 'vitest'
 
 import Mounter from './fixtures/Mounter.svelte'
-import { IS_HAPPYDOM, IS_SVELTE_5 } from './utils.js'
 
 const onMounted = vi.fn()
 const onDestroyed = vi.fn()
 const renderSubject = () => render(Mounter, { onMounted, onDestroyed })
 
-describe.skipIf(IS_SVELTE_5 && IS_HAPPYDOM)('mount and destroy', () => {
+describe('mount and destroy', () => {
   test('component is mounted', async () => {
     renderSubject()
 
