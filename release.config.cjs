a9747b492654f82ab72df54b8f312eb698ce1c1f
Michael Cousins <michael@cousins.io>
2024-04-24 10:23:01 -0400

    ci(release): configure semantic release plugin in dry-run mode

---
+++ release.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+}
