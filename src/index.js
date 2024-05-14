06e3fafe8f02e64f80d0c2b692376dad7e62bcb5
Michael Cousins <michael@cousins.io>
2024-05-14 16:11:29 -0400

    fixup: remove pure export, concentrate on test fixes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import { act, cleanup } from './pure.js'
 // If we're running in a test runner that supports afterEach
 // then we'll automatically run cleanup afterEach test
 // this ensures that tests run in isolation from each other
-// if you don't like this then either import the `pure` module
-// or set the STL_SKIP_AUTO_CLEANUP env variable to 'true'.
+// if you don't like this then set the STL_SKIP_AUTO_CLEANUP env variable.
 if (typeof afterEach === 'function' && !process.env.STL_SKIP_AUTO_CLEANUP) {
   afterEach(async () => {
     await act()
