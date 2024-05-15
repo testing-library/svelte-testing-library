ac3248d0e92188f747936cc5e975c76b52a3b199
Michael Cousins <michael@cousins.io>
2024-05-15 12:09:21 -0400

    test: fix auto-cleanup tests (#371)

--- src/__tests__/auto-cleanup.test.js
+++ src/__tests__/auto-cleanup.test.js
@@ -1,31 +1,42 @@
-import { render } from '@testing-library/svelte'
-import { describe, expect, test } from 'vitest'
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
 
-import Comp from './fixtures/Comp.svelte'
+import { IS_SVELTE_5 } from './utils.js'
+
+const importSvelteTestingLibrary = async () =>
+  IS_SVELTE_5 ? import('../svelte5-index.js') : import('../index.js')
+
+const globalAfterEach = vi.fn()
 
 describe('auto-cleanup', () => {
-  // This just verifies that by importing STL in an
-  // environment which supports afterEach (like jest)
-  // we'll get automatic cleanup between tests.
-  test('first', () => {
+  beforeEach(() => {
+    vi.resetModules()
+    globalThis.afterEach = globalAfterEach
+  })
+
+  afterEach(() => {
+    delete process.env.STL_SKIP_AUTO_CLEANUP
+    delete globalThis.afterEach
+  })
+
+  test('calls afterEach with cleanup if globally defined', async () => {
+    const { render } = await importSvelteTestingLibrary()
+
+    expect(globalAfterEach).toHaveBeenCalledTimes(1)
+    expect(globalAfterEach).toHaveBeenLastCalledWith(expect.any(Function))
+    const globalCleanup = globalAfterEach.mock.lastCall[0]
+
+    const { default: Comp } = await import('./fixtures/Comp.svelte')
     render(Comp, { props: { name: 'world' } })
+    await globalCleanup()
+
+    expect(document.body).toBeEmptyDOMElement()
   })
 
-  test('second', () => {
-    expect(document.body.innerHTML).toEqual('')
-  })
-})
+  test('does not call afterEach if process STL_SKIP_AUTO_CLEANUP is set', async () => {
+    process.env.STL_SKIP_AUTO_CLEANUP = 'true'
 
-describe('cleanup of two components', () => {
-  // This just verifies that by importing STL in an
-  // environment which supports afterEach (like jest)
-  // we'll get automatic cleanup between tests.
-  test('first', () => {
-    render(Comp, { props: { name: 'world' } })
-    render(Comp, { props: { name: 'universe' } })
-  })
+    await importSvelteTestingLibrary()
 
-  test('second', () => {
-    expect(document.body.innerHTML).toEqual('')
+    expect(globalAfterEach).toHaveBeenCalledTimes(0)
   })
 })
