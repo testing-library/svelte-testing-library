90ba912d4609d36168732a39e7fe6f99a35330a1
Michael Cousins <michael@cousins.io>
2024-11-19 12:20:36 -0500

    fix(types): do not erase component type constraint (#410)

--- src/component-types.d.ts
+++ src/component-types.d.ts
@@ -1,17 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-redundant-type-constituents */
-import type * as Svelte from 'svelte'
+import type {
+  Component as ModernComponent,
+  ComponentConstructorOptions as LegacyConstructorOptions,
+  ComponentProps,
+  EventDispatcher,
+  mount,
+  SvelteComponent as LegacyComponent,
+  SvelteComponentTyped as Svelte3LegacyComponent,
+} from 'svelte'
 
-type IS_MODERN_SVELTE = Svelte.Component extends (...args: any[]) => any
+type IS_MODERN_SVELTE = ModernComponent extends (...args: any[]) => any
   ? true
   : false
 
+type IS_LEGACY_SVELTE_4 =
+  EventDispatcher<any> extends (...args: any[]) => any ? true : false
+
 /** A compiled, imported Svelte component. */
 export type Component<
-  P extends Record<string, any>,
-  E extends Record<string, any>,
+  P extends Record<string, any> = any,
+  E extends Record<string, any> = any,
 > = IS_MODERN_SVELTE extends true
-  ? Svelte.Component<P, E> | Svelte.SvelteComponent<P>
-  : Svelte.SvelteComponent<P>
+  ? ModernComponent<P, E> | LegacyComponent<P>
+  : IS_LEGACY_SVELTE_4 extends true
+    ? LegacyComponent<P>
+    : Svelte3LegacyComponent<P>
 
 /**
  * The type of an imported, compiled Svelte component.
@@ -19,12 +32,12 @@ export type Component<
  * In Svelte 5, this distinction no longer matters.
  * In Svelte 4, this is the Svelte component class constructor.
  */
-export type ComponentType<C> = IS_MODERN_SVELTE extends true
-  ? C
-  : new (...args: any[]) => C
+export type ComponentType<C> = C extends LegacyComponent
+  ? new (...args: any[]) => C
+  : C
 
 /** The props of a component. */
-export type Props<C extends Component<any, any>> = Svelte.ComponentProps<C>
+export type Props<C extends Component> = ComponentProps<C>
 
 /**
  * The exported fields of a component.
@@ -32,9 +45,9 @@ export type Props<C extends Component<any, any>> = Svelte.ComponentProps<C>
  * In Svelte 5, this is the set of variables marked as `export`'d.
  * In Svelte 4, this is simply the instance of the component class.
  */
-export type Exports<C> = C extends Svelte.SvelteComponent
+export type Exports<C> = C extends LegacyComponent
   ? C
-  : C extends Svelte.Component<any, infer E>
+  : C extends ModernComponent<any, infer E>
     ? E
     : never
 
@@ -43,7 +56,6 @@ export type Exports<C> = C extends Svelte.SvelteComponent
  *
  * In Svelte 4, these are the options passed to the component constructor.
  */
-export type MountOptions<C extends Component<any, any>> =
-  IS_MODERN_SVELTE extends true
-    ? Parameters<typeof Svelte.mount<Props<C>, Exports<C>>>[1]
-    : Svelte.ComponentConstructorOptions<Props<C>>
+export type MountOptions<C extends Component> = C extends LegacyComponent
+  ? LegacyConstructorOptions<Props<C>>
+  : Parameters<typeof mount<Props<C>, Exports<C>>>[1]
