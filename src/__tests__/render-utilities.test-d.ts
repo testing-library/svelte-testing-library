6f563c36065dd84de6b6e32d4d7cb2684c3d0420
Michael Cousins <michael@cousins.io>
2025-05-07 11:29:02 -0400

    test: replace deprecated `toMatchTypeOf`

--- src/__tests__/render-utilities.test-d.ts
+++ src/__tests__/render-utilities.test-d.ts
@@ -8,7 +8,7 @@ describe('render query and utility types', () => {
   test('render result has default queries', () => {
     const result = subject.render(Component, { name: 'Alice' })
 
-    expectTypeOf(result.getByRole).parameters.toMatchTypeOf<
+    expectTypeOf(result.getByRole).parameters.toExtend<
       [role: subject.ByRoleMatcher, options?: subject.ByRoleOptions]
     >()
   })
@@ -27,25 +27,25 @@ describe('render query and utility types', () => {
       { queries: { getByVibes } }
     )
 
-    expectTypeOf(result.getByVibes).parameters.toMatchTypeOf<[vibes: string]>()
+    expectTypeOf(result.getByVibes).parameters.toExtend<[vibes: string]>()
   })
 
   test('act is an async function', () => {
-    expectTypeOf(subject.act).toMatchTypeOf<() => Promise<void>>()
+    expectTypeOf(subject.act).toExtend<() => Promise<void>>()
   })
 
   test('act accepts a sync function', () => {
-    expectTypeOf(subject.act).toMatchTypeOf<(fn: () => void) => Promise<void>>()
+    expectTypeOf(subject.act).toExtend<(fn: () => void) => Promise<void>>()
   })
 
   test('act accepts an async function', () => {
-    expectTypeOf(subject.act).toMatchTypeOf<
+    expectTypeOf(subject.act).toExtend<
       (fn: () => Promise<void>) => Promise<void>
     >()
   })
 
   test('fireEvent is an async function', () => {
-    expectTypeOf(subject.fireEvent).toMatchTypeOf<
+    expectTypeOf(subject.fireEvent).toExtend<
       (
         element: Element | Node | Document | Window,
         event: Event
@@ -54,7 +54,7 @@ describe('render query and utility types', () => {
   })
 
   test('fireEvent[eventName] is an async function', () => {
-    expectTypeOf(subject.fireEvent.click).toMatchTypeOf<
+    expectTypeOf(subject.fireEvent.click).toExtend<
       (
         element: Element | Node | Document | Window,
         // eslint-disable-next-line @typescript-eslint/no-empty-object-type
