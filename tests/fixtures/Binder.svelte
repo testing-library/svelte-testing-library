82132c19266fb2281d5217b7caf9379ee485565f
Michael Cousins <michael@cousins.io>
2026-06-21 15:21:37 -0400

    fix(core): fix props proxy target to avoid double `$effect` runs
    Fixes #493

---
+++ tests/fixtures/Binder.svelte
@@ -0,0 +1,12 @@
+<script>
+  let { onEffectRun, value = $bindable(false) } = $props()
+
+  let checked = $state()
+
+  $effect(() => {
+    value = checked
+    onEffectRun?.(checked)
+  })
+</script>
+
+<input type="checkbox" bind:checked />
